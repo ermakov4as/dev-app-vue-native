531f280504ee5c29692d9b6c1e83cbd3fad07fd6
ermakov4as <ermakov4as@gmail.com>
2019-03-19 00:21:17 +0300

    v0.0.0.6

--- app/components/Trainer.vue
+++ app/components/Trainer.vue
@@ -41,45 +41,69 @@
                         <StackLayout v-else-if="showPlayer">
                             <StackLayout class="border-red">
                                 <StackLayout class="border-imitate">
-                                    <label textWrap="true" :text="cardName + ` (${currentNum}/${maxNum})`" 
-                                            class="h2 description-label no-bottom-mr" />
-                                    <FlexboxLayout  class="switch-line" flexDirection="row-reverse">
-                                        <Label text="EN-RU" />
-                                        <Switch v-model="enRu" />
-                                        <Label text="RU-EN" />
-                                    </FlexboxLayout>
 
-                                    <template v-if="enRu">
-                                        <FlexboxLayout  class="label-margin">
-                                            <Image src="~/assets/images/audio-volume.png" stretch="aspectFit" class="icon" width="50" 
-                                                    @tap="playAudio(sentence.question_audio)" v-if="sentence.question_audio != null" />
-                                            <Label :text="sentence.question" />
+                                    <template v-if="!cardSuccessMode">
+                                        <label textWrap="true" :text="cardName + ` (${currentNum}/${maxNum})`" 
+                                                class="h2 description-label no-bottom-mr" />
+                                        <label v-if="cardSaved" textWrap="true" text="Карточка успешно сохранена!" 
+                                                class="h2 description-label no-bottom-mr success-label" />
+                                        <FlexboxLayout  class="switch-line" flexDirection="row-reverse">
+                                            <Label text="EN-RU" />
+                                            <Switch v-model="enRu" />
+                                            <Label text="RU-EN" />
                                         </FlexboxLayout>
-                                        <FlexboxLayout class="label-margin">
-                                            <Image src="~/assets/images/audio-volume.png" stretch="aspectFit" class="icon" width="50" 
-                                                    @tap="playAudio(sentence.answer_audio)" v-if="sentence.answer_audio != null" />
-                                            <Label :text="sentence.answer" />
-                                        </FlexboxLayout>
+
+                                        <template v-if="enRu">
+                                            <FlexboxLayout  class="label-margin">
+                                                <Image src="~/assets/images/audio-volume.png" stretch="aspectFit" class="icon" width="50" 
+                                                        v-show="showA" v-if="sentence.question_audio != null"
+                                                        @tap="playAudio(sentence.question_audio)" />
+                                                <Label v-if="sentence.question != null" :text="sentence.question" v-show="showA" />
+                                                <Label text="" />
+                                            </FlexboxLayout>
+                                            <FlexboxLayout class="label-margin">
+                                                <Image src="~/assets/images/audio-volume.png" stretch="aspectFit" class="icon" width="50" 
+                                                        v-show="showB" v-if="sentence.answer_audio != null"
+                                                        @tap="playAudio(sentence.answer_audio)" />
+                                                <Label v-if="sentence.answer != null" :text="sentence.answer" v-show="showB" />
+                                                <Label text="" />
+                                            </FlexboxLayout>
+                                        </template>
+
+                                        <template v-else>
+                                            <FlexboxLayout class="label-margin">
+                                                <Image src="~/assets/images/audio-volume.png" stretch="aspectFit" class="icon" width="50" 
+                                                        v-show="showA" v-if="sentence.answer_audio != null"
+                                                        @tap="playAudio(sentence.answer_audio)" />
+                                                <Label v-if="sentence.answer != null" :text="sentence.answer" v-show="showA" />
+                                                <Label text="" />
+                                            </FlexboxLayout>
+                                            <FlexboxLayout  class="label-margin">
+                                                <Image src="~/assets/images/audio-volume.png" stretch="aspectFit" class="icon" width="50" 
+                                                        v-show="showB" v-if="sentence.question_audio != null"
+                                                        @tap="playAudio(sentence.question_audio)" />
+                                                <Label v-if="sentence.question != null" :text="sentence.question" v-show="showB"/>
+                                                <Label text="" />
+                                            </FlexboxLayout>
+                                        </template>
+
+                                        <GridLayout rows="auto" columns="*, *, *">
+                                            <Button text="<-" @tap="prevSentence" class="btn-common btn-white" row="0" col="0" />
+                                            <Button text="Далее" @tap="further" class="btn-common btn-green" row="0" col="1" />
+                                            <Button text="->" @tap="nextSentence" class="btn-common btn-white" row="0" col="2" />
+                                        </GridLayout>
                                     </template>
 
                                     <template v-else>
-                                        <FlexboxLayout class="label-margin">
-                                            <Image src="~/assets/images/audio-volume.png" stretch="aspectFit" class="icon" width="50" 
-                                                    @tap="playAudio(sentence.answer_audio)" v-if="sentence.answer_audio != null" />
-                                            <Label :text="sentence.answer" />
-                                        </FlexboxLayout>
-                                        <FlexboxLayout  class="label-margin">
-                                            <Image src="~/assets/images/audio-volume.png" stretch="aspectFit" class="icon" width="50" 
-                                                    @tap="playAudio(sentence.question_audio)" v-if="sentence.question_audio != null" />
-                                            <Label :text="sentence.question" />
-                                        </FlexboxLayout>
+                                        <label textWrap="true" :text="`Отметить карточку '${cardName}' как прослушанную?`" 
+                                                class="h2 description-label no-bottom-mr" />
+                                        <label v-if="cardSaveError" textWrap="true" 
+                                                text="Ошибка при сохранении" class="h2 description-label error-label" />
+                                        <GridLayout rows="auto" columns="*, *">
+                                            <Button text="Да" @tap="cardSuccess" class="btn-big btn-green" row="0" col="0" />
+                                            <Button text="Нет" @tap="cardSuccessReject" class="btn-big btn-orange" row="0" col="1" />
+                                        </GridLayout>
                                     </template>
-
-                                    <GridLayout rows="auto" columns="*, *, *">
-                                        <Button text="<-" @tap="prevSentence" class="btn-common btn-white" row="0" col="0" />
-                                        <Button text="Далее" @tap="further" class="btn-common btn-green" row="0" col="1" />
-                                        <Button text="->" @tap="nextSentence" class="btn-common btn-white" row="0" col="2" />
-                                    </GridLayout>
 
                                 </StackLayout>
                             </StackLayout>
@@ -112,8 +136,10 @@
             return {
                 title: "Тренажёр",
                 loading: true,
+                preLoading: 0,
                 loadError: false,
                 cardTitle: "",
+                cardId: null,
                 lessons: [],
                 cards: [],
                 currentNum: 0,
@@ -124,33 +150,90 @@
                 showPlayer: false,
                 sentences: [],
                 sentence: {},
-                enRu: true
+                enRu: true,
+                futherStage: 0,
+                showA: false,
+                showB: false,
+                cardSuccessMode: false,
+                cardSaved: false,
+                cardSaveError: false
             };
         },
         
         methods: {
-            further() {
+            cardSuccessReject() {
+                this.cardSuccessMode = false;
+                this.cardSaveError = false;
+            },
 
+            cardSuccess() {
+                this.loading = true;
+                HTTP.post(`player/card/${this.cardId}/success/`)
+                    .then((response) => {
+                        this.cardSaved = true;
+                        this.cardSuccessMode = false;
+                        this.loading = false;
+                    })
+                    .catch(error => {
+                        this.cardSaveError = true;
+                        this.loading = false;
+                        console.dir(error);
+                    });
+                
+            },
+
+            further() {
+                this.futherStage += 1;
+                if (this.futherStage === 1) {
+                    this.showA = true;
+                } else if (this.futherStage === 2) {
+                    if (this.enRu) {
+                        if (this.sentence.question_audio !== null) this.playAudio(this.sentence.question_audio)
+                        else this.further();
+                    } else {
+                        if (this.sentence.answer_audio !== null) this.playAudio(this.sentence.answer_audio)
+                        else this.further();
+                    }
+                } else if (this.futherStage === 3) {
+                    this.showB = true;
+                } else if (this.futherStage === 4) {
+                    if (!this.enRu) {
+                        if (this.sentence.question_audio !== null) this.playAudio(this.sentence.question_audio)
+                        else this.further();
+                    } else {
+                        if (this.sentence.answer_audio !== null) this.playAudio(this.sentence.answer_audio)
+                        else this.further();
+                    }
+                } else if (this.futherStage === 5) {
+                    this.nextSentence();
+                }
             },
 
             prevSentence() {
+                this.cardSaved = false;
                 if (this.currentNum > 1) {
                     this.loading = true;
                     this.sentence = this.sentences[this.currentNum - 2];
                     this.currentNum -= 1;
+                    this.futherStage = 0;
+                    this.showA = false;
+                    this.showB = false;
                     this.loading = false;
                 }
             },
 
             nextSentence() {
+                this.cardSaved = false;
                 if (this.currentNum < this.maxNum) {
                     this.loading = true;
                     this.sentence = this.sentences[this.currentNum];
                     this.currentNum += 1;
-                    if (this.currentNum === this.maxNum) {
-                        // TODO: /player/card/{id}/success/ (POST)
-                    }
+                    this.futherStage = 0;
+                    this.showA = false;
+                    this.showB = false;
                     this.loading = false;
+                } else if (this.currentNum === this.maxNum) {
+                    this.cardSuccessMode = true;    
                 }
             },
 
@@ -182,6 +265,8 @@
 
             reward(mode) {
                 this.loading = true;
+                this.cardSaved = false;
+                this.cardSaveError = false;
                 if (mode === 'cards-lessons') {
                     this.showLessons = true;
                     this.showCards = false;
@@ -198,11 +283,11 @@
             cardChecked(id) {
                 this.cards.forEach((card, i, cards) => {
                     if (card.id === id) {
-                        //console.dir(card);
                         this.maxNum = Number.parseInt(card.sentence_count);
                         this.cardName = card.name;
                         this.loading = true;
                         this.showCards = false;
+                        this.cardId = id;
                         this.getPlayerCard(id);
                     }
                 });
@@ -211,14 +296,11 @@
             lessonChecked(id) {
                 this.lessons.forEach((lesson, i, lessons) => {
                     if (lesson.id === id) {
-                        //console.dir(lesson);
                         this.cards = lesson.cards;
-                        //this.mode = 'cards'
                         this.loading = true;
                         this.showLessons = false;
                         this.showCards = true;
                         this.loading = false;
-                        console.dir(this.cards);
                     }
                 });
             },
@@ -226,18 +308,15 @@
             getPlayerCard(id) {
                 HTTP.get(`player/card/${id}/`)
                     .then((response) => {
-                        this.showPlayer = true; // TODO: /player/settings/
-                        this.loading = false;
+                        this.showPlayer = true;
                         this.sentences = response.data.sentences;
                         this.sentence = response.data.sentences[0];
                         this.currentNum = 1;
-                        /*console.dir('SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS');
-                        console.dir(response.data);
-                        console.dir('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF');*/
+                        this.preLoading += 1;
                     })
                     .catch(error => {
-                        this.loading = false;
                         this.loadError = true;
+                        this.loading = false;
                         console.dir(error);
                     });
             },
@@ -246,23 +325,51 @@
                 HTTP.get('player/playlist/')
                     .then((response) => {
                         this.lessons = response.data.lessons;
-                        //this.mode = 'lessons';
                         this.showLessons = true;
                         this.loading = false;
-                        /*console.dir('SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS');
-                        console.dir(response.data);
-                        console.dir('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF');*/
                     })
                     .catch(error => {
-                        this.loading = false;
                         this.loadError = true;
+                        this.loading = false;
+                        console.dir(error);
+                    });
+            },
+
+            getSettings() {                             // TODO: что означает несколько объектов в массивах ruen и enru?
+                HTTP.get('player/settings/')            // TODO: соответственно, сделать получение и сохранение
+                    .then((response) => {
+                        console.dir('SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS');
+                        console.dir(response.data);
+                        console.dir('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF');
+                        this.preLoading += 1;
+                    })
+                    .catch(error => {
+                        this.loadError = true;
+                        this.loading = false;
                         console.dir(error);
                     });
             }
         },
 
+        watch: {
+            enRu: {                             // TODO: и что здесь должно быть?
+                handler(val, oldVal) {
+                    console.dir(this.enRu);
+                }
+            },
+
+            preLoading: {
+                handler(val, oldVal) {
+                    if (this.preLoading === 2) {
+                        this.loading = false;
+                    }
+                }
+            }
+        },
+
         created() {
             this.getPlaylist();
+            this.getSettings();
         }
     };
 </script>
